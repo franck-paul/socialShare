00aeba656237ea7660a36b83e7627af8b635271f
franck <carnet.franck.paul@gmail.com>
2017-11-13 12:12:29 +0100

    Release 0.4.1

--- _define.php
+++ _define.php
@@ -16,7 +16,7 @@ $this->registerModule(
 	/* Name */				"socialShare",
 	/* Description*/		"Add social networks sharing buttons to your posts and pages",
 	/* Author */			"Franck Paul, Kozlika",
-	/* Version */			'0.4',
+	/* Version */			'0.4.1',
 	array(
 		/* Dependencies */	'requires' =>		array(array('core','2.9')),
 		/* Permissions */	'permissions' =>	'admin',
