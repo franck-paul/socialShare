4baf8b3b394e9ff66a720a3dbca4c7b5f1b088d5
franck <carnet.franck.paul@gmail.com>
2018-03-09 14:25:56 +0100

    Various code formatting

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of socialShare, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief socialShare, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 if (!defined('DC_CONTEXT_ADMIN')) {return;}
 
