97df1f4aad35ca1f06793badc7244f7e046b7d17
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:50 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,18 +10,22 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "socialShare",                                                 // Name
-    "Add social networks sharing buttons to your posts and pages", // Description
-    "Franck Paul, Kozlika",                                        // Author
+    'socialShare',                                                 // Name
+    'Add social networks sharing buttons to your posts and pages', // Description
+    'Franck Paul, Kozlika',                                        // Author
     '1.1',                                                         // Version
     [
         'requires'    => [['core', '2.13']],                     // Dependencies
         'permissions' => 'admin',                                // Permissions
-        'support'     => 'https://open-time.net/?q=socialShare', // Support URL
-        'type'        => 'plugin'                               // Type
+        'type'        => 'plugin',                               // Type
+
+        'details'    => 'https://open-time.net/?q=socialShare',       // Details URL
+        'support'    => 'https://github.com/franck-paul/socialShare', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/socialShare/main/dcstore.xml'
     ]
 );
