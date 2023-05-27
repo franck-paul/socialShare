f29bef35d25c2dde84c3e85f19805526c3598f34
franck <carnet.franck.paul@gmail.com>
2023-05-27 16:08:16 +0200

    Release 3.0.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'socialShare',
     'Add social networks sharing buttons to your posts and pages',
     'Franck Paul, Kozlika',
-    '3.0.2',
+    '3.0.3',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
