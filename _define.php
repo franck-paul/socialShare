d1f174e78a41a0c3472ed5e569934e988b60fb88
franck <carnet.franck.paul@gmail.com>
2023-05-27 16:07:47 +0200

    Typo

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'socialShare',
     'Add social networks sharing buttons to your posts and pages',
     'Franck Paul, Kozlika',
-    '3.0.1',
+    '3.0.2',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
--- src/Manage.php
+++ src/Manage.php
@@ -198,7 +198,7 @@ class Manage extends dcNsProcess
         $radio_styles = [];
         $i            = 0;
         foreach ($ssb_use_styles as $k => $v) {
-            $radio_styles[] = (new para())->items([
+            $radio_styles[] = (new Para())->items([
                 (new Radio(['ssb_use_style', 'ssb_use_style_' . $i], $ssb_use_style == $k))
                     ->value($k)
                     ->label((new Label($v, Label::INSIDE_TEXT_AFTER))),
